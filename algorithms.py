1a082c9caff95c80730df84b5201bc1ed870a26f
Murus <muradmustafayev03@gmail.com>
2020-05-27 11:02:53 +0400

    added new functions, changed file structure

--- algorithms.py
+++ algorithms.py
@@ -1,38 +1,6 @@
 import math
 
 
-def prime_factorization(num):
-    res = []
-    i = 2
-    while i <= num:
-        if num % i == 0:
-            res.append(i)
-            num = num / i
-        else:
-            i += 1
-
-    return res
-
-
-def euler_func(num):
-    res = 0
-    num_pf = prime_factorization(num)
-    for i in range(1, num):
-        if not set(prime_factorization(i)) & set(num_pf):
-            res += 1
-
-    return res
-
-
-def find_all_divisors(num):
-    res = []
-    for i in range(1, num):
-        if num % i == 0:
-            res.append(i)
-
-    return res
-
-
 def is_prime(num):
     if num == 1:
         return False
@@ -62,3 +30,87 @@ def _is_prime_(num):  # more accurate, but works slowly
         return False
 
     return True
+
+
+def prime_factorization(num):
+    result = []
+    i = 2
+    while i <= num:
+        if num % i == 0:
+            result.append(i)
+            num = num / i
+        else:
+            i += 1
+
+    return result
+
+
+def euler_func(num):
+    result = 0
+    num_pf = prime_factorization(num)
+    for i in range(1, num):
+        if not set(prime_factorization(i)) & set(num_pf):
+            result += 1
+
+    return result
+
+
+def find_all_divisors(num):
+    result = []
+    for i in range(1, num):
+        if num % i == 0:
+            result.append(i)
+
+    return result
+
+
+def multiples(divisor, ceiling):
+    result = []
+    for i in range(ceiling):
+        if i % divisor == 0:
+            result.append(i)
+
+    return result
+
+
+def fibonacci_sequence(ceiling, start=1):
+    current = start
+    previous = start
+
+    result = []
+
+    while current < ceiling:
+        result.append(current)
+        sum = previous + current
+        previous = current
+        current = sum
+
+    return result
+
+
+def remove_odds(array):
+    i = 0
+
+    while i < len(array):
+        if array[i] % 2 != 0:
+            array.pop(i)
+        else:
+            i += 1
+
+    return array
+
+
+def remove_evens(array):
+    i = 0
+
+    while i < len(array):
+        if array[i] % 2 == 0:
+            array.pop(i)
+        else:
+            i += 1
+
+    return array
+
+
+
+
